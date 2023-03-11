856a31024682e32bc2b8e96157e749294d0c6489
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-03-11 13:55:07 +0000

    added scaffolding for Next.js app

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Cryptoverse',
+    favicon: '/favicon.ico'
+}
+
+export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }

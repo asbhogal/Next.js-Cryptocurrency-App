acf8c97cc1888036c8f3d532398cb566acf47f6c
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-25 22:06:07 +0000

    Removes prop, adds metadata for SEO

--- app/exchanges/page.tsx
+++ app/exchanges/page.tsx
@@ -1,4 +1,10 @@
 import Exchanges from "@/components/Exchanges";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Exchanges | Cryptoverse",
+  description: "The latest cryptocurrency exchange rates",
+};
 
 export default function page() {
   return <Exchanges />;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-"use client";
-
-const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "Cryptoverse | Dashboard",
+  description:
+    "View cryptocurrency stats, market cap and top 10 cryptocurrencies, as well as browse the statistics of many others",
 };
 
 import { Dashboard } from "@/components";

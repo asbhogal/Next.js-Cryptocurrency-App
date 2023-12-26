3fbadff53eebb1afb6bbdfe9ce4b9b43f6fec2d2
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-12-26 02:29:47 +0000

    Adds further a11y tests (PASS), improves navbar accessibility

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,26 +17,26 @@ const Navbar = () => {
       </div>
       <ul className="NavList">
         <li className="NavListItem">
-          <Link href="/">
+          <Link href="/" aria-label="Home">
             <BiHomeAlt2 size={28} />
             <p>Home</p>
           </Link>
         </li>
         <li className="NavListItem">
-          <Link href="/crypto-currencies">
-            <BsCurrencyBitcoin size={28} />
+          <Link href="/crypto-currencies" aria-label="Cryptocurrencies">
+            <BsCurrencyBitcoin size={28} aria-hidden="true" />
             <p>Cryptocurrencies</p>
           </Link>
         </li>
         <li className="NavListItem">
-          <Link href="/exchanges">
-            <BsCurrencyExchange size={28} />
+          <Link href="/exchanges" aria-label="Exchanges">
+            <BsCurrencyExchange size={28} aria-hidden="true" />
             <p>Exchanges</p>
           </Link>
         </li>
         <li className="NavListItem">
-          <Link href="/news">
-            <BsNewspaper size={28} />
+          <Link href="/news" aria-label="News">
+            <BsNewspaper size={28} aria-hidden="true" />
             <p>News</p>
           </Link>
         </li>
--- tests/a11y.spec.ts
+++ tests/a11y.spec.ts
@@ -12,3 +12,39 @@ test.describe("homepage", () => {
     expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
+
+test.describe("cryptocurrencies", () => {
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    await page.goto("/crypto-currencies");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+test.describe("exchanges", () => {
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    await page.goto("/exchanges");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+test.describe("news", () => {
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    await page.goto("/news");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
